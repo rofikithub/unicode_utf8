8196268c8608775c370ab746207e62c1b8f0f83a
Md. Rafikul Islam (Rofik) <156637357+rofikithub@users.noreply.github.com>
2024-02-19 22:30:17 +0600

    Create main.py

---
+++ main.py
@@ -0,0 +1,18 @@
+import json
+
+from ftfy import fix_encoding
+
+def json_file_print(url):
+    with open(url, 'r') as f:
+        data = json.load(f)
+
+    for i in range(len(data)):
+        print(data[i]["id"]+"="+fix_encoding(data[i]["name"]))
+
+
+def main():
+    json_file_print("localhost.json")
+
+
+if __name__ == "__main__":
+    main()
